660132c7f97d86eb7940d6baf10cf4d8625ccf50
DanielSololidEdu <92855790+DanielSololidEdu@users.noreply.github.com>
2022-09-16 10:54:12 +0200

    Add files via upload

---
+++ EdufyAllMedia/EdufyAllMedia/src/main/java/com/edufy/EdufyAllMedia/EdufyAllMediaApplication.java
@@ -0,0 +1,13 @@
+package com.edufy.EdufyAllMedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdufyAllMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdufyAllMediaApplication.class, args);
+	}
+
+}
